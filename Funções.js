cac876b6c42dde09556315958d4871ec347e0ab9
BRIAN FELIPE GONCALVES <brian.7908@aluno.pr.senac.br>
2024-02-14 17:27:02 -0300

    +Exercicios

---
+++ Funções.js
@@ -0,0 +1,6 @@
+function somar (par1, par2) {
+    const res = par1 + par2;
+    console.log (res);
+}
+
+somar(1, 2);
---
+++ Operadores.js
@@ -0,0 +1,11 @@
+var soma = 2+2;
+var subtracao = 10-4;
+var multiplicacao = 10*10;
+var divisao = 10/10;
+var modulo = 10%7;
+
+console.log("Resultado da soma:", soma);
+console.log("Resultado da subtração:", subtracao);
+console.log("Resultado da multiplicação:", multiplicacao);
+console.log("Resultado da divisão:", divisao);
+console.log("Resultado do módulo:", modulo);
